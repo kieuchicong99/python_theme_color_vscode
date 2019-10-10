9ba149ed4dad56380b948faa171433139932a3d2
kieuchicong99 <kieuchicong99@gmail.com>
2019-10-10 07:12:14 +0700

    update

---
+++ pytheme/test.py
@@ -0,0 +1,20 @@
+### design by ckc
+
+# this is a comment
+import pandas
+
+
+def helloWorld():
+    print ("hello world")
+    a=[]
+
+def sum(a , b):
+    Sum = a + b
+    return Sum 
+
+def blabla(a, b , c):
+    flag = True
+    print("blabla") if True == True else print("ok")
+if __name__ == "__main__":
+    blabla()
+
